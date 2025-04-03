3f56c93a805f7ef23aeb9148bfdf135080835bb3
Claudio Beatrice <claudi0.beatric3@gmail.com>
2025-04-04 00:25:50 +0200

    feat: add back gh-release drafting

---
+++ scripts/draft-gh-release.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -e
+set -o errexit -o nounset
+
+KIND="${1}"
+IMAGE="${2}"
+VERSION="${3}"
+
+IMAGE_LC=$(echo "${IMAGE}" | tr '[:upper:]' '[:lower:]')
+VERSION_LC=$(echo "${VERSION}" | tr '[:upper:]' '[:lower:]')
+
+if ! gh release view ${IMAGE_LC}-${VERSION_LC} &>/dev/null; then
+    gh release create \
+        --generate-notes \
+        --draft=true \
+        --title="${IMAGE} ${VERSION}" \
+        "${IMAGE_LC}-${VERSION_LC}" \
+        "dist/${KIND}/${IMAGE_LC}/${VERSION_LC}/reports" \
+        "dist/${KIND}/${IMAGE_LC}/${VERSION_LC}/sboms"
+fi
