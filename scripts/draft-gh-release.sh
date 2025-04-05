6b2ac873d468e07902ee752c623aac42bb6f896c
Claudio Beatrice <claudi0.beatric3@gmail.com>
2025-04-05 16:21:57 +0200

    chore: tweak gh release draft script

--- scripts/draft-gh-release.sh
+++ scripts/draft-gh-release.sh
@@ -10,7 +10,11 @@ VERSION="${3}"
 IMAGE_LC=$(echo "${IMAGE}" | tr '[:upper:]' '[:lower:]')
 VERSION_LC=$(echo "${VERSION}" | tr '[:upper:]' '[:lower:]')
 
-if ! gh release view ${IMAGE_LC}-${VERSION_LC} &>/dev/null; then
+if gh release view "${IMAGE_LC}-${VERSION_LC}" >/dev/null 2>&1; then
+    echo "Release ${IMAGE_LC}-${VERSION_LC} already exists."
+else
+    echo "Creating draft release ${IMAGE_LC}-${VERSION_LC}..."
+
     gh release create \
         --generate-notes \
         --draft=true \
