deb653d065287582ed2bfa173ebbaaf7c4785068
Claudio Beatrice <claudi0.beatric3@gmail.com>
2025-04-20 00:55:08 +0200

    feat(ytt): port checkmake gh workflow to ytt

--- scripts/generate-updatecli-config.sh
+++ scripts/generate-updatecli-config.sh
@@ -3,6 +3,17 @@
 set -e
 set -o errexit -o nounset
 
+find ytt/.github/workflows/*.yml -maxdepth 1 -type f -name "*.yml" ! -name "*.lib.yml" | while read -r src; do
+    dst=$(echo "${src}" | sed 's/ytt\///g' | sed 's/.yml/.yaml/g')
+
+    ytt \
+        -f "ytt/.github/workflows/fragments.lib.yml" \
+        -f "${src}" | \
+        yq --indent=2 | \
+        sed 's/^\"on\"\:$/on:/' | \
+        sed 's/^  workflow_dispatch: null$/  workflow_dispatch:/' > "${dst}"
+done
+
 find ytt/updatecli/updatecli.d/*.yml -maxdepth 1 -type f -name "*.yml" ! -name "*.lib.yml" | while read -r src; do
     dst=$(echo "${src}" | sed 's/ytt\///g' | sed 's/.yml/.yaml/g')
 
