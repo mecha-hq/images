4c04f60b0fe0b6f3ebb5d04b47cb6d803d60f2e1
Claudio Beatrice <claudi0.beatric3@gmail.com>
2025-07-12 15:32:07 +0200

    feat: add ytt config to generate updatecli config for deps, separate configs per scope

--- scripts/generate-updatecli-config.sh
+++ scripts/generate-updatecli-config.sh
@@ -3,8 +3,10 @@
 set -e
 set -o errexit -o nounset
 
-find ytt/updatecli/updatecli.d/*.yml -maxdepth 1 -type f -name "*.yml" ! -name "*.lib.yml" | while read -r src; do
+find ytt/updatecli/updatecli.d/**/*.yml -type f -name "*.yml" ! -name "*.lib.yml" | while read -r src; do
     dst=$(echo "${src}" | sed 's/ytt\///g' | sed 's/.yml/.yaml/g')
+
+    mkdir -p "$(dirname ${dst})"
 
     ytt \
         -f "ytt/updatecli/updatecli.d/fragments.lib.yml" \
