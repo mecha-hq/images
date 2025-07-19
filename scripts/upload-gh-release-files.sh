5a874bea90939cbf88557ca0543b876031f13687
Claudio Beatrice <claudi0.beatric3@gmail.com>
2025-07-20 00:21:05 +0200

    fix: change the way file uploads are done

--- scripts/upload-gh-release-files.sh
+++ scripts/upload-gh-release-files.sh
@@ -10,16 +10,32 @@ VERSION="${3}"
 IMAGE_LC=$(echo "${IMAGE}" | tr '[:upper:]' '[:lower:]')
 VERSION_LC=$(echo "${VERSION}" | tr '[:upper:]' '[:lower:]')
 REPORTS_DIR="dist/${KIND}/${IMAGE}/${VERSION}/reports/"
+TAG="${IMAGE_LC}-${VERSION_LC}"
 
 if [ -z "${REPORTS_DIR}" ]; then
     echo "No reports directory found"
     exit 1
 fi
 
-REPORTS_FILES=$(find "${REPORTS_DIR}" -type f -printf "%p " 2>/dev/null)
+echo "Uploading files to release ${TAG}..."
 
-echo "Uploading files to release ${IMAGE_LC}-${VERSION_LC}..."
+RELEASE_ID=$(gh api \
+    -H "Accept: application/vnd.github+json" \
+    /repos/mecha-hq/images/releases/tags/"${TAG}" \
+    --jq '.id'
+)
 
-gh release upload "${IMAGE_LC}-${VERSION_LC}" ${REPORTS_FILES} \
-    --repo mecha-hq/images \
-    --clobber
+for FILE in $(find "${REPORTS_DIR}" -type f); do
+    FILENAME=$(basename "${FILE}")
+
+    echo "Uploading ${FILENAME}..."
+
+    curl -sSL \
+        -X POST \
+        -H "Authorization: token ${GITHUB_TOKEN}" \
+        -H "Content-Type: $(file --brief --mime-type "${FILE}")" \
+        --data-binary @"${FILE}" \
+        "https://uploads.github.com/repos/mecha-hq/images/releases/${RELEASE_ID}/assets?name=${FILENAME}&label=${FILENAME}"
+done
+
+echo "All files uploaded successfully."
